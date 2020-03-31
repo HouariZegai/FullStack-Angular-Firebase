c065ff830582eb3e35756d556df702faf0aa8ba5
HouariZegai <houarizegai14@gmail.com>
2020-03-31 10:50:37 +0100

    Front-end - generate component & making the router

--- e-commerce-front-end/src/app/app-routing.module.ts
+++ e-commerce-front-end/src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { GoodsComponent } from './components/goods/goods.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "login", component: LoginComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "cart", component: CartComponent},
+  {path: "admin", component: GoodsComponent},
+  {path: "**", component: NotFoundComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- e-commerce-front-end/src/app/app.module.ts
+++ e-commerce-front-end/src/app/app.module.ts
@@ -1,13 +1,27 @@
+import { NavComponent } from './components/nav/nav.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { GoodsComponent } from './components/goods/goods.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    SignupComponent,
+    GoodsComponent,
+    NotFoundComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
