271b13d2722a66f9c7f52e0e9b2c377c4785f696
HouariZegai <houarizegai14@gmail.com>
2020-03-31 11:21:30 +0100

    Front-end - making menu (navbar links to pages)

--- e-commerce-front-end/src/app/components/nav/nav.component.ts
+++ e-commerce-front-end/src/app/components/nav/nav.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  isOpen: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  tglNavbar() {
+    this.isOpen = !this.isOpen
   }
 
 }
