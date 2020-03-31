dd263d994746a9d14cf34b1dd912d16ae07d8fbf
HouariZegai <houarizegai14@gmail.com>
2020-03-31 13:46:06 +0100

    Front-end - making home page (& impl product cards)

--- e-commerce-front-end/src/app/components/home/home.component.ts
+++ e-commerce-front-end/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../interfaces/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products: Array<Product> = []
+
   constructor() { }
 
   ngOnInit(): void {
+    this.products.push(
+      {name: "apple", price: 120, imgUrl: "assets/images/apple.jpg"},
+      {name: "Banana", price: 270, imgUrl: "assets/images/banana.jpg"},
+      {name: "Pineapple", price: 600, imgUrl: "assets/images/pineapple.jpg"},
+      {name: "Strawberry", price: 300, imgUrl: "assets/images/strawberry.jpg"}
+    )
+  }
+
+  addToCart(index) {
+    console.log(this.products[index])
   }
 
 }
